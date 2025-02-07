fec8ec491239cb51670c817045d64f3a851ee611
+ <+>
2025-02-07 12:00:53 +0300

    Object Inheritance Lab Part One

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,10 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = ["Teaching is fun!"]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
